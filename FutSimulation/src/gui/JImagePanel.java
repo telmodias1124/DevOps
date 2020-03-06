04f45074b7c71702b254aea3ca7c216a5a30dd45
1998a <1998a@DESKTOP-K4EF2DC>
2020-03-06 14:18:25 +0100

    Modification du remplissage lors de la phase éliminatoire

--- FutSimulation/src/gui/JImagePanel.java
+++ FutSimulation/src/gui/JImagePanel.java
@@ -20,7 +20,7 @@ public class JImagePanel extends JPanel {
 	private String name;
 	private String score;
 	private boolean onlyRec;
-	Font tr = new Font("SansSerif", Font.BOLD, 16);
+	Font tr = new Font("SansSerif", Font.BOLD, 12);
 	
 	public JImagePanel(String path) {
 		this.name = null;
@@ -56,18 +56,27 @@ public class JImagePanel extends JPanel {
     super.paintComponent(g); 
     if (name != null) {
     	g.setFont(tr);
-    	g.setColor(Color.BLACK);
     	if(!onlyRec) {
+
+        	g.setColor(Color.BLACK);
+        	g.fillRect(0, this.getHeight()/2 - 30, this.getWidth(), this.getHeight()/2);
+
+        	g.setColor(Color.WHITE);
 	    	g.drawImage(image, 0,this.getHeight()/2 - 15 ,30,30,this); 	
 	    	g.drawString(name, 35, this.getHeight()/2 + 5);
 	    	g.drawString(score, this.getWidth() - 10 - (score.length()*5), this.getHeight()/2 + 5);
+    	}else {
+    		g.setColor(Color.BLACK);	
+    		g.fillRect(0, this.getHeight()/2 - 30, this.getWidth(), this.getHeight()/2);
     	}
+    	
     	Stroke stroke = new BasicStroke(5, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] { 3, 0 }, 0);
     	g2.setStroke(stroke);
-    	g.drawLine(0, this.getHeight()/2 - 30, this.getWidth(), this.getHeight()/2 - 30);
+    	
+    	/*g.drawLine(0, this.getHeight()/2 - 30, this.getWidth(), this.getHeight()/2 - 30);
         g.drawLine(0, this.getHeight()/2 - 30 + 60, this.getWidth(), this.getHeight()/2 - 30 + 60);
         g.drawLine(0,this.getHeight()/2 - 30 ,0, this.getHeight()/2 - 30 + 60);
-        g.drawLine(this.getWidth()-1,this.getHeight()/2 - 30 ,this.getWidth()-1, this.getHeight()/2 - 30 + 60);
+        g.drawLine(this.getWidth()-1,this.getHeight()/2 - 30 ,this.getWidth()-1, this.getHeight()/2 - 30 + 60);*/
     }
     else {
     	g.drawImage(image, 0,0,this.getWidth(),this.getHeight(),this);
